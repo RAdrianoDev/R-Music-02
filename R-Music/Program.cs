d0f3a2d85bbfdd0267ef149cbe2ac92c04fa5cf0
Ronaldo Adriano Ferreira <ronaldo@radriano.com>
2023-07-05 10:56:01 -0400

    Project Alura C# course 2023-07-05_01

---
+++ R-Music/Program.cs
@@ -0,0 +1,15 @@
+﻿Music music1 = new Music();
+music1.musicName = "Thousand Miles Away";
+music1.singerName = "Fei Yu-Ching";
+music1.lengthTime = 261;
+music1.WriteAvailability(true);
+
+
+Music music2 = new Music();
+music2.musicName = "Unstoppable";
+music2.singerName = "Sia";
+music2.lengthTime = 218;
+music2.WriteAvailability(false);
+
+music1.ShowDataSheet();
+music2.ShowDataSheet();
---
+++ R-Music/music.cs
@@ -0,0 +1,39 @@
+﻿//describing a music. It should have:
+//name - musicName
+//artist - singer
+//music length - lengthTime
+//if available on the plan or not - availability
+
+class Music
+{
+    public string musicName;
+    public string singerName;
+    public int lengthTime;
+    private bool availability;
+
+
+    public void WriteAvailability(bool value)
+    {
+        availability = value;
+    }
+
+    public bool Availability() 
+    {
+        return availability;
+    }
+
+    public void ShowDataSheet()
+    {
+        Console.WriteLine($"Music name: {musicName}");
+        Console.WriteLine($"Singer name: {singerName}");
+        Console.WriteLine($"Music Length: {lengthTime}");
+        if (availability)
+        {
+            Console.WriteLine("This music is available on your plan!");
+        } else
+        {
+            Console.WriteLine("Not available. Update your plan.");
+        }
+        
+    }
+}
